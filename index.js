1f412caa98d1cc7d778fda3e3c9161714bc7a9bd
J Zhang <jzhangbusiness01@gmail.com>
2022-11-16 16:30:50 -0500

    v 1.0

--- index.js
+++ index.js
@@ -7,21 +7,21 @@ const port = 3000;
 
 const tmdb = require("./tmdb");
 
-app.use(express.json());
-const whitelist = ["http://127.0.0.1", "http://127.0.0.1:5500"];
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  optionsSuccessStatus: 200
-};
+// app.use(express.json());
+// const whitelist = ["http://127.0.0.1", "http://127.0.0.1:5500"];
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     if (!origin || whitelist.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   optionsSuccessStatus: 200
+// };
 
-app.use(cors(corsOptions));
-// app.use(cors());
+// app.use(cors(corsOptions));
+app.use(cors());
 
 app.get("/", (req, res) => res.json({ success: "Hello World!" }));
 app.use("/tmdb", tmdb);
